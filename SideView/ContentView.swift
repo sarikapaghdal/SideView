925f8a0c3db691bb418609e5f64d15cd00f28685
Dhaval Shekhada <sarika.paghdal1@gmail.com>
2022-03-02 17:08:20 +0100

    Initial Commit

---
+++ SideView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideView
+//
+//  Created by Sarika on 02.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideView/SideViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideViewApp.swift
+//  SideView
+//
+//  Created by Sarika on 02.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SideViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
